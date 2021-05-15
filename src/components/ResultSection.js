d0489fd5d393b95e7d9d5cf5e56d8779f881def7
ddalf <twiidi_@naver.com>
2021-05-15 13:47:22 +0900

    Add : No search result view

--- src/components/ResultSection.js
+++ src/components/ResultSection.js
@@ -7,7 +7,6 @@ export default class ResultSection{
     this.section = document.createElement('section');
     this.section.className = 'results-section';
     $target.appendChild(this.section);
-    this.render();
   }
 
   setState(data){
@@ -16,9 +15,8 @@ export default class ResultSection{
   }
 
   render(){
-    if(this.data){
       this.section.innerHTML = '';
-
+      if(this.data.length > 0){
       const cardContainer = document.createElement('div');
       cardContainer.className = 'card-container';
 
@@ -31,6 +29,22 @@ export default class ResultSection{
       });
 
       this.section.appendChild(cardContainer);
+    }
+    else{
+        const noticeSection = document.createElement('section');
+        noticeSection.className = 'noticeSection';
+
+        const notice = document.createElement('h2');
+        notice.className = 'notice';
+        notice.innerText = '검색 결과가 없습니다.';
+
+        const noticeImage = document.createElement('img');
+        noticeImage.className = 'notice-image';
+        noticeImage.src = 'src/img/emptybox.png';
+
+        noticeSection.appendChild(notice);
+        noticeSection.appendChild(noticeImage);
+        this.section.appendChild(noticeSection);
     }
   }
 }
